2c8137b9fc8c3b051fbc8474f7a1413a21c484e7
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-11-12 22:58:25 +0100

    Changed some values minMax

--- AirTrafficController/Game1.cs
+++ AirTrafficController/Game1.cs
@@ -51,11 +51,11 @@ namespace AirTrafficController
         private bool gameRunning = true;
 
         //Min/Max of values
-        public static int[] minMaxAcceleration = {1, 10};
-        public static int[] minMaxMAXSpeed = {200, 1200};//Max speed
-        public static int[] minMaxAltitude = {200, 9000};
+        public static int[] minMaxAcceleration = {0, 20};
+        public static int[] minMaxMAXSpeed = {1, 1200};//Max speed
+        public static int[] minMaxAltitude = {1, 9000};
         public static int[] minMaxCapacity = {20, 500};
-        public static int[] minMaxVerticalAcceleration = {-10, 10};
+        public static int[] minMaxVerticalAcceleration = {-20, 20};
         //
         private iAirplane closestAirplaneToMouse;
 
